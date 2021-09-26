2546b21d0ebd8f91be46f3e1e43c94bf0ab8f655
sun.sun <sun.sun@ingcreations.com>
2021-09-26 13:54:47 +0800

    路由动画

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="singer" v-loading="!singers.length">
         <IndexList :data="singers" @select="selectSinger"></IndexList>
-        <router-view :singer="selectedSinger"></router-view>
+        <!-- <router-view :singer="selectedSinger"></router-view> -->
+        <router-view v-slot="{ Component }">
+            <transition appear name="slide">
+                <component :is="Component" :singer="selectedSinger"/>
+            </transition>
+        </router-view>
     </div>
 </template>
 <script>
