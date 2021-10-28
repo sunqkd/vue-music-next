475e11245abcd72951158e394702df91e7875a58
sunzhaoqkd <sun.sun@ingcreations.com>
2021-10-28 11:29:13 +0800

    '高阶scroll组件'

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -56,7 +56,7 @@
 
 <script>
   import SongList from '@/components/base/song-list/song-list'
-  import Scroll from '@/components/base/scroll/scroll'
+  import Scroll from '@/components/wrap-scroll'
   import { mapActions, mapState } from 'vuex'
   const RESERVED_HEIGHT = 40
 
--- src/components/wrap-scroll/index.js
+++ src/components/wrap-scroll/index.js
@@ -1,12 +1,13 @@
 // 高阶 scroll组件
-import { h, mergeProps, withCtx, renderSlot, ref } from 'vue'
+import { h, mergeProps, withCtx, renderSlot, ref, computed, watch, nextTick } from 'vue'
 import Scroll from '@/components/base/scroll/scroll'
+import { useStore } from 'vuex'
 
 export default {
     name: 'wrap-scroll',
     props: Scroll.props,
     emits: Scroll.emits,
-    // ctx 上下文
+    // ctx 上下文 没用到模板， 使用render函数实现
     render(ctx) {
         return h(Scroll, mergeProps({ ref: 'scrollRef' }, ctx.$props, {
             onScroll: (e) => {
@@ -20,8 +21,20 @@ export default {
     },
     setup() {
         const scrollRef = ref(null)
+        // 利用计算属性做延时
+        const scroll = computed(() => {
+            return scrollRef.value.scroll
+        })
+        const store = useStore()
+        const playList = computed(() => store.state.playList)
+        // 歌曲列表变更，更新（重新计算滚动高度）
+        watch(playList, async() => {
+            await nextTick()
+            scroll.value.refresh()
+        })
         return {
-            scrollRef
+            scrollRef,
+            scroll
         }
     }
 }
