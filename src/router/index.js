7bb2d440e9135a5c17a400ff53e7d587f9a3367e
sunzhaoqkd <sun.sun@ingcreations.com>
2021-11-17 11:39:45 +0800

    '路由异步加载模块'

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Recommend from '@/views/recommend'
-import Singer from '@/views/singer'
-import TopList from '@/views/top-list'
-import Search from '@/views/search'
-import SingerDetail from '@/views/singer-detail'
-import Album from '@/views/album'
-import TopDetail from '@/views/top-detail'
-import UserCenter from '@/views/user-center'
+// import Recommend from '@/views/recommend'
+// import Singer from '@/views/singer'
+// import TopList from '@/views/top-list'
+// import Search from '@/views/search'
+// import SingerDetail from '@/views/singer-detail'
+// import Album from '@/views/album'
+// import TopDetail from '@/views/top-detail'
+// import UserCenter from '@/views/user-center'
+
+// 使用工厂函数形式，异步加载，可以加载注释（模数注释），更改分隔打包后名称,注释后打包后为名字，而非数字
+
+const Recommend = () => import('@/views/recommend'/* webpackChunkName:"recommend" */)
+const Singer = () => import('@/views/singer'/* webpackChunkName:"singer" */)
+const TopList = () => import('@/views/top-list'/* webpackChunkName:"toplist" */)
+const Search = () => import('@/views/search'/* webpackChunkName:"search" */)
+const SingerDetail = () => import('@/views/singer-detail'/* webpackChunkName:"singerdetail" */)
+const Album = () => import('@/views/album'/* webpackChunkName:"album" */)
+const TopDetail = () => import('@/views/top-detail'/* webpackChunkName:"topdetail" */)
+const UserCenter = () => import('@/views/user-center'/* webpackChunkName:"usercenter" */)
+
 const routes = [
   {
     path: '/',
--- vue.config.js
+++ vue.config.js
@@ -20,5 +20,11 @@ module.exports = {
     before(app) {
       registerRouter(app)
     }
+  },
+  configureWebpack: (config) => {
+    if (process.env.npm_config_report) {
+      const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+      config.plugins.push(new BundleAnalyzerPlugin())
+    }
   }
 }
